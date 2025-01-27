b05eb856cac2269d8c8d82e8952b6eac598b4d33
Longhao Li <longhao_li@outlook.com>
2025-01-27 23:10:14 +0800

    Add SystemErrorCode for Windows.

---
+++ include/onion/error.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace onion {
+
+/// \class SystemErrorCode
+/// \brief
+///   Wrapper class for system error code.
+class SystemErrorCode {
+public:
+    /// \brief
+    ///   Construct a \c SystemErrorCode that represents no error.
+    constexpr SystemErrorCode() noexcept : m_code{0} {}
+
+    /// \brief
+    ///   Construct a \c SystemErrorCode with the given error code.
+    /// \param code
+    ///   System error code value.
+    constexpr SystemErrorCode(std::int32_t code) noexcept : m_code{code} {}
+
+    /// \brief
+    ///   Checks if this \c SystemErrorCode represents an error.
+    /// \retval true
+    ///   This \c SystemErrorCode represents no error.
+    /// \retval false
+    ///   This \c SystemErrorCode represents an error.
+    [[nodiscard]]
+    constexpr auto ok() const noexcept -> bool {
+        return m_code == 0;
+    }
+
+    /// \brief
+    ///   Get error code value of this \c SystemErrorCode. 0 means no error.
+    /// \return
+    ///   Error code value of this \c SystemErrorCode.
+    [[nodiscard]]
+    constexpr auto value() const noexcept -> std::int32_t {
+        return m_code;
+    }
+
+    /// \brief
+    ///   Get error message of this \c SystemErrorCode.
+    /// \return
+    ///   Error message of this \c SystemErrorCode.
+    [[nodiscard]]
+    ONION_API auto message() const noexcept -> std::string;
+
+    /// \brief
+    ///   Check if this \c SystemErrorCode is equal to another \c SystemErrorCode.
+    /// \param other
+    ///   The \c SystemErrorCode to compare with.
+    /// \retval true
+    ///   This \c SystemErrorCode is equal to \c other.
+    /// \retval false
+    ///   This \c SystemErrorCode is not equal to \c other.
+    [[nodiscard]]
+    constexpr auto operator==(SystemErrorCode other) const noexcept -> bool {
+        return m_code == other.m_code;
+    }
+
+    /// \brief
+    ///   Compare this \c SystemErrorCode with another \c SystemErrorCode.
+    /// \param other
+    ///   The \c SystemErrorCode to compare with.
+    /// \retval std::strong_ordering::equal
+    ///   This \c SystemErrorCode is equal to \c other.
+    /// \retval std::strong_ordering::less
+    ///   This \c SystemErrorCode is less than \c other.
+    /// \retval std::strong_ordering::greater
+    ///   This \c SystemErrorCode is greater than \c other.
+    [[nodiscard]]
+    constexpr auto operator<=>(SystemErrorCode other) const noexcept -> std::strong_ordering {
+        return m_code <=> other.m_code;
+    }
+
+private:
+    /// \brief
+    ///   System error code. For Windows, this is the value returned by \c GetLastError(); For other
+    ///   platforms, this is the value of \c errno.
+    std::int32_t m_code;
+};
+
+/// \struct SystemIoResult
+/// \brief
+///   Result of a system I/O operation.
+struct [[nodiscard]] SystemIoResult {
+    /// \brief
+    ///   Error code of the system I/O operation.
+    SystemErrorCode error;
+
+    /// \brief
+    ///   Size of the data transferred in bytes. This value is valid only if \c error is not an
+    ///   error.
+    std::uint32_t size;
+};
+
+} // namespace onion
---
+++ src/onion/error.cpp
@@ -0,0 +1,28 @@
+#include "onion/error.hpp"
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+#    ifndef WIN32_LEAN_AND_MEAN
+#        define WIN32_LEAN_AND_MEAN
+#    endif
+#    ifndef NOMINMAX
+#        define NOMINMAX
+#    endif
+#    include <Windows.h>
+#endif
+
+#include <array>
+
+using namespace onion;
+
+auto SystemErrorCode::message() const noexcept -> std::string {
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+    std::array<char, 1024> buffer;
+
+    DWORD length =
+        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr,
+                       static_cast<DWORD>(m_code), MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+                       buffer.data(), static_cast<DWORD>(buffer.size()), nullptr);
+
+    return {buffer.begin(), buffer.begin() + length};
+#endif
+}
